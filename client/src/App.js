fe1e3c1fdd57c6cd4ee3cf8b6fb8b1e1bf0245ed
Sylvia Engmann <sylvia.engmann8@gmail.com>
2022-06-10 23:11:04 +0000

    added parts to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+import React , { useEffect, useState } from 'react';
 import './App.css';
+import { Card, Button} from 'react-bootstrap';
 
-function App() {
+const URL = "127.0.0.1:5000"
+
+const cardStyling = {
+  backgroundColor: "white",
+  color: "black",
+  border: "5px solid black",
+  borderRadius: "2px",
+  width: "50rem",
+  height: "150px",
+  margin: "10px"
+}
+
+const buttonStyling = {
+  backgroundColor: "grey",
+  width: "10rem",
+}
+
+const App = () => {
+  let [baseCurrencies, setBaseCurrencies] = useState([]);
+  let [targetCurrencies, setTargetCurrencies] = useState([]);
+  //let [selectedBase]
+  let [exchangeRate, setExchangeRate] = useState({})
+  let [amountToConvert, setAmountToConvert] = useState([]);
+
+  useEffect(() => {
+    //getBaseCurrency();
+    //getTargetCurrency();
+  }, []);
+
+   async function getBaseCurrency () {
+    try {
+      let response = await fetch(`${URL}/base`);
+      if (response.ok) {
+        let base =  await response.json();
+        setBaseCurrencies(base);
+      } else {
+        console.log(`Server error: ${response.status} ${response.statusText}`);
+      }
+    } catch (err) {
+      console.log(`Network error: ${err.message}`);
+    }
+  }
+
+   async function getTargetCurrency() {
+    try {
+      let response = fetch(`${URL}/target`);
+      if (response.ok) {
+        let target =  response.json();
+        setTargetCurrencies(target);
+      } else {
+        console.log(`Server error: ${response.status} ${response.statusText}`);
+      }
+    } catch (err) {
+      console.log(`Network error: ${err.message}`);
+    }
+  }
+
+  async function getExchangeRate (baseCurrency, targetCurrency) {
+    try {
+      let response = await fetch(`${URL}/xrate`);
+      if (response.ok) {
+        let exchangerate =  await response.json();
+        setExchangeRate(exchangerate);
+      } else {
+        console.log(`Server error: ${response.status} ${response.statusText}`);
+      }
+    } catch (err) {
+      console.log(`Network error: ${err.message}`);
+    }
+  }
+
+   function convertAmount() {
+     //getExchangeRate(baseCurrency, targetCurrency);
+     convertedAmount(exchangeRate, amountToConvert)
+  }
+  let convertedAmount = (exchangeRate, amount ) => {return exchangeRate * amount }
+
+  const selectBaseValues = baseCurrencies.map((e) => 
+   <select key={e.id}>{e.symbol}</select>
+  )
+
+  const selectTargetValues = targetCurrencies.map((e) => 
+   <select key={e}>{e}</select>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Crypto Currency Converter</h2>
+      <Card style={cardStyling}>
+        <p>Select the Base Currency</p>
+        {selectBaseValues}
+        
+      </Card>
+
+      <Card style={cardStyling}>
+        <p>Select the Target Currency</p>
+        {selectTargetValues}
+      </Card>
+
+      <Card style={cardStyling}>
+        <p>How much do you want to convert?</p>
+        <input type="number"/>
+
+        <Button style={buttonStyling} onClick="convertAmount()"> Convert </Button>
+      </Card>
+
+      <Card>Converted Amount is {}</Card>
     </div>
   );
 }
